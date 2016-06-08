bfaa54d6bd8e1c9c83540ab8a196073eb05d44a6
radhakrishna.s <radhakrishna.s@turvo.com>
2016-06-08 19:42:57 +0530

    First commit

---
+++ apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+/usr/bin/puppet apply /etc/puppet/manifests/site.pp --modulepath /etc/puppet/modules:/etc/puppet/plugins
+if [ $? -eq 0 ] ; then
+  /usr/bin/logger -i "Puppet ran successfully" -t "puppet-apply"
+  exit 0
+else
+  /usr/bin/logger -i "Puppet encountered an error; please run puppet manually" -t "puppet-apply"
+  exit 1
+fi
